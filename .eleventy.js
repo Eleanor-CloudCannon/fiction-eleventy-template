f63ecadfc94cd818fd576d605f25fc50964da42e
Eleanor <eleanor@cloudcannon.com>
2023-05-17 11:38:13 +1200

    feat: head and nav without css

--- .eleventy.js
+++ .eleventy.js
@@ -6,7 +6,20 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPlugin(pluginBookshop({
     bookshopLocations: ["component-library"],
     pathPrefix: '',
-  }));
+  })
+  );
 
   // ...
+  eleventyConfig.ignores.add("site/schemas");
+  eleventyConfig.addPassthroughCopy("site/css");
+  eleventyConfig.addPassthroughCopy("site/fonts");
+  eleventyConfig.addPassthroughCopy("site/images");
+  eleventyConfig.addPassthroughCopy("site/js");
+  eleventyConfig.addPassthroughCopy("site/vendor");
+  return {
+    dir: {
+      input: "site",
+      pages: "pages",
+    },
+  };
 };
---
+++ site/pages/index.md
@@ -0,0 +1,6 @@
+---
+permalink: /
+title: Home
+layout: base.liquid
+---
+Hello, I'm an Opossum....
